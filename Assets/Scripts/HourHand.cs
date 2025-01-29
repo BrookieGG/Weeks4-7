777cbc79e76bec44ecf37aba46a5c09c0a83c4d9
DESKTOP-SAL2D94\brook <brooke4444@me.com>
2025-01-28 22:01:28 -0500

    Got the clock chime to play on every rotation and a bird to appear everytime the sound plays

--- Assets/Scripts/HourHand.cs
+++ Assets/Scripts/HourHand.cs
@@ -6,29 +6,36 @@ public class HourHand : MonoBehaviour
 {
     public AudioClip clip;
     public float hourhand = 0.01f;
-    //public float hour;
     public AudioSource audioSource;
+    float z;
+    public GameObject prefab;
+    public GameObject spawner;
+    private float lastSpawned = 0f;
+    private float t;
     // Start is called before the first frame update
     void Start()
     {
-        
+        lastSpawned = t;
     }
 
     // Update is called once per frame
     void Update()
     {
+       // Vector3 spawnerPosition = spawner.transform.position;
+        t += Time.deltaTime;
         Vector3 rot = transform.eulerAngles;
         rot.z += hourhand;
         transform.eulerAngles = rot;
+        z = rot.z % 360;
 
-        if (rot.z == -30)
+
+        if (Mathf.Abs(z % 30) < 0.1f && t - lastSpawned > 1f)
         {
             audioSource.PlayOneShot(clip);
-        }
-        if (rot.z == -60)
-        {
-            audioSource.PlayOneShot(clip);
-        }
+            lastSpawned = t;
+            GameObject spawned = Instantiate(prefab, spawner.transform.position, Quaternion.identity);
+            Destroy(spawned, 0.5f);
 
+        }
     }
 }
