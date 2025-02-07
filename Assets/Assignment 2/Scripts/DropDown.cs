38ba0d2734af98888578cfb6c53b3232b9975faa
DESKTOP-SAL2D94\brook <brooke4444@me.com>
2025-02-06 21:01:18 -0500

    Set Up

---
+++ Assets/Assignment 2/Scripts/DropDown.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DropDown : MonoBehaviour
+{
+    public TMP_Dropdown dropdown;
+    public SpriteRenderer mySprite;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        mySprite.sprite = dropdown.options[0].image;
+    }
+    // Update is called once per frame
+    public void OnValueChanged(int index)
+    {
+        Debug.Log(dropdown.options[index].text);
+        mySprite.sprite = dropdown.options[index].image;
+    }
+}
