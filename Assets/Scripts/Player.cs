42fe93dcf3af1eb5d08bde044636567bd1aa60fb
DESKTOP-SAL2D94\brook <brooke4444@me.com>
2025-02-12 16:44:51 -0500

    Got the collisons to work!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,25 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     public float speed = 5;
-    //private float playerStart = 0;
+    public GameObject Win;
+    private bool playing = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Win.SetActive(false);
+        playing = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 pos = transform.position;
+        if (playing == true)
+        {
+            Vector2 pos = transform.position;
 
-        pos.y += Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
-        transform.position = pos;
+            pos.y += Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+            transform.position = pos;
+        }
+        if (playing == false && Input.GetKeyDown(KeyCode.Space))
+        {
+            Win.SetActive(false);
+            playing = true;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -29,6 +41,11 @@ public class Player : MonoBehaviour
             Debug.Log("Try Again!");
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 
+        }
+        if (collision.tag == "Win")
+        {
+            playing = false;
+            Win.SetActive(true);
         }
     }
 }
