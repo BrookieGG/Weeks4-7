f29d7e6c14295e9caed033a1065abcfe52ac5798
DESKTOP-SAL2D94\brook <brooke4444@me.com>
2025-02-14 17:02:24 -0500

    Slider works just needs to be scaled

--- Assets/Assignment 2/Scripts/PlanetBehaviour.cs
+++ Assets/Assignment 2/Scripts/PlanetBehaviour.cs
@@ -10,14 +10,20 @@ public class PlanetBehaviour : MonoBehaviour
     public Slider speedSlider;
     public Slider sizeSlider;
     public SpriteRenderer renderer;
+    public SpriteRenderer preview;
     public TMP_Dropdown dropdown;
+    private float size;
     // Start is called before the first frame update
     void Start()
     {
         spawn.onClick.AddListener(Spawn);
         speedSlider.onValueChanged.AddListener(delegate { SetSpeed(); });
         sizeSlider.onValueChanged.AddListener(delegate { SetSize(); });
-
+        sizeSlider.minValue = 0f;
+        sizeSlider.maxValue = 10f;
+        size = sizeSlider.value;
+        size = 130f;
+        SetSize();
     }
 
     // Update is called once per frame
@@ -42,7 +48,13 @@ public class PlanetBehaviour : MonoBehaviour
     //Changes the size of the planet
     void SetSize()
     {
-        renderer.transform.localScale = new Vector3(sizeSlider.value,  sizeSlider.value, sizeSlider.value);
+        size = sizeSlider.value + 100f;
+        float scale = size / 100f;
+        renderer.size = new Vector2(size, size);
+        preview.transform.localScale = new Vector2(scale * 10 , scale * 10 );
+        
+        
+
     }
 
 }
