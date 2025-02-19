3c63f8777f622a2cd116039f08071fd78e833c53
DESKTOP-SAL2D94\brook <brooke4444@me.com>
2025-02-19 13:54:50 -0500

    Pressure Plate activates the spikes which kills the player on collision

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -1,28 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class Spike : MonoBehaviour
+public class PressurePlate : MonoBehaviour
 {
-    
+    public GameObject spike;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spike.SetActive(false);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
-            //resart game
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            //activate the spike
+            spike.SetActive(true);
+           
         }
     }
-
-
-
     // Update is called once per frame
     void Update()
     {
