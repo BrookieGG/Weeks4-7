2e906c3f62728862c94bb0c3b0651980afe2a354
DESKTOP-SAL2D94\brook <brooke4444@me.com>
2025-02-21 13:25:47 -0500

    Added a second UI panel

--- Assets/Assignment 2/Scripts/DropDownSelect.cs
+++ Assets/Assignment 2/Scripts/DropDownSelect.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class DropDownSelect : MonoBehaviour
 {
+    public TMP_Dropdown dropdown;
+    public SpriteRenderer mySprite;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        mySprite.sprite = dropdown.options[0].image;
+    }
+    // Update is called once per frame
+    public void OnValueChanged(int index)
+    {
+        Debug.Log(dropdown.options[index].text);
+        mySprite.sprite = dropdown.options[index].image;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Assignment 2/Scripts/PlanetBehaviour.cs
+++ Assets/Assignment 2/Scripts/PlanetBehaviour.cs
@@ -9,34 +9,63 @@ public class PlanetBehaviour : MonoBehaviour
     public Button spawn;
     public Slider speedSlider;
     public Slider sizeSlider;
-    public SpriteRenderer renderer;
+    public SpriteRenderer[] renderers;
     public SpriteRenderer preview;
     public TMP_Dropdown dropdown;
+    public bool[] isActive;
+    public float[] speeds;
+
     private float size;
+    private int index = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         spawn.onClick.AddListener(Spawn);
         speedSlider.onValueChanged.AddListener(delegate { SetSpeed(); });
         sizeSlider.onValueChanged.AddListener(delegate { SetSize(); });
-        sizeSlider.minValue = 0f;
-        sizeSlider.maxValue = 10f;
+        sizeSlider.minValue = 130f;
+        sizeSlider.maxValue = 260f;
         size = sizeSlider.value;
-        size = 130f;
+        index = 0;
+        isActive = new bool[10];
+        speeds = new float[10];
+
         SetSize();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            if (isActive[i])
+            {
+                Vector3 pos = renderers[i].transform.position;
+                pos.x += (speeds[i] * Time.deltaTime);
+                renderers[i].transform.position = pos;
+                if (pos.x > 45f) //if off of the screen set active to false
+                {
+                    isActive[i] = false;
+                }
+            }
+        }
     }
 
     //Spawn planet onto the map
     void Spawn()
     {
+        SpriteRenderer renderer = renderers[index];
         renderer.sprite = dropdown.options[dropdown.value].image;
-        renderer.transform.position = new Vector3(0,0,0);
+        renderer.transform.position = new Vector3(-45, Random.Range(-19f, 19f), 0);
+        renderer.transform.localScale = new Vector2(size / 13, size / 13);
+        isActive[index] = true;
+        speeds[index] = speedSlider.value;
+
+
+
+
+        index = (index + 1) % renderers.Length;
     }
 
     //Changes the speed of the planet
@@ -48,12 +77,9 @@ public class PlanetBehaviour : MonoBehaviour
     //Changes the size of the planet
     void SetSize()
     {
-        size = sizeSlider.value + 100f;
-        float scale = size / 100f;
-        renderer.size = new Vector2(size, size);
-        preview.transform.localScale = new Vector2(scale * 10 , scale * 10 );
-        
-        
+        size = sizeSlider.value;
+        //renderer.size = new Vector2(size, size);
+        preview.transform.localScale = new Vector2(size, size);
 
     }
 
